bcc3ecccdffe93706d7c3727ad8f9546c49f003c
Benjamin Trettin <ben@2022-Eli.Bryan>
2018-05-11 16:24:06 -0500

    Added tests for login/signup modals

--- src/test/HomePage.spec.js
+++ src/test/HomePage.spec.js
@@ -2,8 +2,26 @@ import React from 'react';
 import { HomePage } from '../components/HomePage';
 import renderer from 'react-test-renderer';
 
-test('Home page looks correct visually', () => {
-    const component = renderer.create(<HomePage/>);
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-});
+describe('Home Page', () => {
+	test('Looks correct visually', () => {
+	    const component = renderer.create(<HomePage/>);
+	    const tree = component.toJSON();
+	    expect(tree).toMatchSnapshot();
+	});
+
+	describe('Login Modal', () => {
+		test('Looks correct visually', () => {
+			const component = renderer.create(<HomePage isLoginModalOpen/>);
+		    const tree = component.toJSON();
+		    expect(tree).toMatchSnapshot();
+		});
+	});
+
+	describe('Signup Modal', () => {
+	test('Looks correct visually', () => {
+			const component = renderer.create(<HomePage isSignUpModalOpen/>);
+		    const tree = component.toJSON();
+		    expect(tree).toMatchSnapshot();
+		});
+	});
+})
