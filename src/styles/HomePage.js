7a848aa29157444f43e90d6be2ebbccaaeeaedc9
Ben Trettin <batrettin@gmail.com>
2018-06-17 18:15:01 -0500

    reformatted the new HomePage page to fit eslint

--- src/styles/HomePage.js
+++ src/styles/HomePage.js
@@ -1,48 +1,44 @@
-const green = '#63BA18';
-const white = '#FFFFFF'
+const green = "#63BA18";
+const white = "#FFFFFF";
 
 export default {
-    navbarContainer: {
-        width: '100%',
-        minHeight: 42,
-        backgroundColor: green
-    },
-<<<<<<< HEAD
-    header: {
-        backgroundImage: "url('lawn.jpg')"
-    },
-    loginSignupMenu: {
-=======
-    loginSignupMenuContainer: {
->>>>>>> f53df091bda135be89eb8e14e6f3a54834c056da
-        paddingRight: '1rem'
-    },
-    menuItem: {
-        color: 'white'
-    },
-    modal: {
-        backgroundColor: 'white',
-        padding: '1rem'
-    },
-    loginGrid: {
-        margin: '1rem'
-    },
-    modalHeader: {
-        display: 'flex',
-        justifyContent: 'center',
-        color: green,
-        fontSize:'2rem',
-        backgroundColor: white,
-        borderBottom: 'none'
-    },
-    modalButton: {
-        backgroundColor: green,
-        color: 'white'
-    },
-    signUpForm: {
-        padding: '1rem'
-    },
-    signUpCheckbox: {
-        paddingBottom: '.5rem'
-    }
+  navbarContainer: {
+    width: "100%",
+    minHeight: 42,
+    backgroundColor: green
+  },
+  header: {
+    backgroundImage: "url('lawn.jpg')"
+  },
+  loginSignupMenu: {
+    paddingRight: "1rem"
+  },
+  menuItem: {
+    color: "white"
+  },
+  modal: {
+    backgroundColor: "white",
+    padding: "1rem"
+  },
+  loginGrid: {
+    margin: "1rem"
+  },
+  modalHeader: {
+    display: "flex",
+    justifyContent: "center",
+    color: green,
+    fontSize: "2rem",
+    backgroundColor: white,
+    borderBottom: "none"
+  },
+  modalButton: {
+    backgroundColor: green,
+    color: "white"
+  },
+  signUpForm: {
+    padding: "1rem"
+  },
+  signUpCheckbox: {
+    paddingBottom: ".5rem"
+  }
 };
