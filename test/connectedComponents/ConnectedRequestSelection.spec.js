098c003efffb5f2d10e9dea94312a4734bff54bb
Shane Drafahl <shanedrafahl@gmail.com>
2018-07-02 22:18:16 -0500

    Added tests and got passing

---
+++ test/connectedComponents/ConnectedRequestSelection.spec.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import configureStore from 'redux-mock-store';
+import RequestSelection from '../../src/connectedComponents/ConnectedRequestSelection';
+
+describe('Connected Home Page', () => {
+  let store;
+
+  beforeEach(() => {
+    const state = {};
+
+    const mockStore = configureStore();
+    store = mockStore(state);
+  });
+
+  it('has the correct props', () => {
+    const wrapper = shallow(<RequestSelection store={store} />);
+
+    const dispatchProps = ['setTypeOfRequest'];
+
+    dispatchProps.forEach((prop) => {
+      expect(wrapper.props()[prop]).toEqual(expect.any(Function));
+    });
+  });
+});
