066742d385b2c95b546241e7916a0fcb50e67947
Ben Trettin <batrettin@gmail.com>
2018-07-19 00:32:17 -0500

    Refactored tests

--- test/components/helpers/DynamicForm.spec.js
+++ test/components/helpers/DynamicForm.spec.js
@@ -16,6 +16,7 @@ import DynamicFormComponent from '../../../src/components/helpers/DynamicForm';
 import InlineErrorComponent from '../../../src/components/helpers/InlineError';
 
 const chance = new Chance();
+const numberOfFields = (chance.integer() % 11) + 1;
 
 const randomType = (useRowCombination) => {
   switch (chance.integer() % 5) {
@@ -66,7 +67,6 @@ const createRandomOptions = () => {
 const generateTestFormJson = () => {
   const name = chance.word();
   const jsonForm = {};
-  const numberOfFields = (chance.integer() % 11) + 1;
   const fields = [];
   for (let counter = 0; counter < numberOfFields; counter += 1) {
     const type = randomType(true);
@@ -121,10 +121,10 @@ const generateTestFormJson = () => {
       }
     }
 
-
     fields[counter] = field;
   }
   jsonForm[name] = {};
+  jsonForm[name].description = chance.word();
   jsonForm[name].fields = fields;
   return jsonForm;
 };
@@ -169,163 +169,174 @@ describe('DynamicForm', () => {
       expect(header.type()).toEqual(Header);
     });
 
-    it('is has a large size', () => {
-      expect(header.props().size).toEqual('large');
+    it('is has an as equal to h3', () => {
+      expect(header.props().as).toEqual('h3');
     });
 
     it('is the correct text in the header', () => {
-      expect(header.childAt(0).debug()).toEqual(Object.keys(testJson)[0]);
+      expect(header.childAt(0).debug()).toEqual(testJson[Object.keys(testJson)[0]].description);
     });
   });
-
-  describe('form', () => {
-    it('is a form', () => {
-      expect(wrapper.childAt(1).type()).toEqual(Form);
-    });
-
-
-    describe('form data', () => {
-      wrapper = renderComponent();
-      testJson[Object.keys(testJson)[0]].fields.forEach((field) => {
-        if (field.validation) {
-          const fail = chance.integer() % 2;
-          let count = 0;
-          const form = wrapper.childAt(1);
-          const segmentOfFields = form.childAt(0);
-          it(`field ${count} is correct`, () => {
-            const label = segmentOfFields.childAt(count).childAt(0);
-            if (field.validation) {
-              expect(label.text()).toEqual(` * ${field.name}`);
-            } else {
-              expect(label.text()).toEqual(field.name);
-            }
-            expect(
-              wrapper
-                .childAt(1)
-                .childAt(0)
-                .childAt(count)
-                .childAt(1)
-                .props().name === field.name,
-            );
-            if (field.type === 'checkbox') {
-              expect(
-                wrapper
-                  .childAt(1)
-                  .childAt(0)
-                  .childAt(count)
-                  .childAt(1)
-                  .type(),
-              ).toEqual(Checkbox);
-            }
-            if (field.type === 'radio') {
-              expect(
-                wrapper
-                  .childAt(1)
-                  .childAt(0)
-                  .childAt(count)
-                  .childAt(1)
-                  .type(),
-              ).toEqual(Radio);
-              failTest(wrapper, field, count);
-            }
-            if (field.type === 'textArea') {
-              expect(
-                wrapper
-                  .childAt(1)
-                  .childAt(0)
-                  .childAt(count)
-                  .childAt(1)
-                  .type(),
-              ).toEqual(TextArea);
-              failTest(wrapper, field, count);
-            }
-            if (field.type === 'dropDown') {
-              expect(
-                wrapper
-                  .childAt(1)
-                  .childAt(0)
-                  .childAt(count)
-                  .childAt(1)
-                  .type(),
-              ).toEqual(Dropdown);
-              failTest(wrapper, field, count);
-            }
-
-            if (field.type === 'rowCombination') {
-              expect(
-                wrapper
-                  .childAt(1)
-                  .childAt(0)
-                  .childAt(count)
-                  .childAt(1)
-                  .type(),
-              ).toEqual(Form.Group);
-
-              let subFieldCounter = 0;
-              field.fields.forEach((input) => {
-                const rowInput = wrapper
-                  .childAt(1)
-                  .childAt(0)
-                  .childAt(count)
-                  .childAt(1)
-                  .childAt(subFieldCounter);
-
-                switch (input.type) {
-                  case 'input':
-                    expect(rowInput.type()).toEqual(Input);
-                    break;
-                  case 'dropDown':
-                    expect(rowInput.type()).toEqual(Dropdown);
-                    break;
-                  case 'textArea':
-                    expect(rowInput.type()).toEqual(TextArea);
-                    break;
-                  case 'checkbox':
-                    expect(rowInput.type()).toEqual(Checkbox);
-                    break;
-                  case 'radio':
-                    expect(rowInput.type()).toEqual(Checkbox);
-                    break;
-                  default:
-                }
-                ifailTest(wrapper, field, count);
-                subFieldCounter += 1;
-              });
-            }
-
-            if (field.type === 'input') {
-              expect(
-                wrapper
-                  .childAt(1)
-                  .childAt(0)
-                  .childAt(count)
-                  .childAt(1)
-                  .type(),
-              ).toEqual(Input);
-
-              failTest(wrapper, field, count);
-            }
-            count += 1;
-          });
-        }
-      });
-    });
-  });
-
-  describe('Submit Button', () => {
-    let submitSegment;
+  describe('form segment', () => {
+    let formSegment;
 
     beforeEach(() => {
-      submitSegment = wrapper.childAt(1).childAt(1);
+      formSegment = wrapper.childAt(1);
     });
 
-    it('It is a segment', () => {
-      expect(submitSegment.type()).toEqual(Segment);
+    it('is a segment', () => {
+      expect(formSegment.type()).toEqual(Segment);
     });
 
-    it('There is a button in the segment', () => {
-      const button = submitSegment.childAt(0);
-      expect(button.type()).toEqual(Button);
+    describe('form', () => {
+      let formComponent;
+      beforeEach(() => {
+        formComponent = formSegment.childAt(0);
+      });
+      it('is a form', () => {
+        expect(formComponent.type()).toEqual(Form);
+      });
+
+      describe('form data', () => {
+        wrapper = renderComponent();
+        testJson[Object.keys(testJson)[0]].fields.forEach((field) => {
+          if (field.validation) {
+            const fail = chance.integer() % 2;
+            let count = 0;
+            const form = wrapper.childAt(1);
+            const segmentOfFields = form.childAt(0);
+            it(`field ${count} is correct`, () => {
+              const label = segmentOfFields.childAt(count).childAt(0);
+              if (field.validation) {
+                expect(label.text()).toEqual(` * ${field.name}`);
+              } else {
+                expect(label.text()).toEqual(field.name);
+              }
+              expect(
+                wrapper
+                  .childAt(1)
+                  .childAt(0)
+                  .childAt(count)
+                  .childAt(1)
+                  .props().name === field.name,
+              );
+              if (field.type === 'checkbox') {
+                expect(
+                  wrapper
+                    .childAt(1)
+                    .childAt(0)
+                    .childAt(count)
+                    .childAt(1)
+                    .type(),
+                ).toEqual(Checkbox);
+              }
+              if (field.type === 'radio') {
+                expect(
+                  wrapper
+                    .childAt(1)
+                    .childAt(0)
+                    .childAt(count)
+                    .childAt(1)
+                    .type(),
+                ).toEqual(Radio);
+                failTest(wrapper, field, count);
+              }
+              if (field.type === 'textArea') {
+                expect(
+                  wrapper
+                    .childAt(1)
+                    .childAt(0)
+                    .childAt(count)
+                    .childAt(1)
+                    .type(),
+                ).toEqual(TextArea);
+                failTest(wrapper, field, count);
+              }
+              if (field.type === 'dropDown') {
+                expect(
+                  wrapper
+                    .childAt(1)
+                    .childAt(0)
+                    .childAt(count)
+                    .childAt(1)
+                    .type(),
+                ).toEqual(Dropdown);
+                failTest(wrapper, field, count);
+              }
+
+              if (field.type === 'rowCombination') {
+                expect(
+                  wrapper
+                    .childAt(1)
+                    .childAt(0)
+                    .childAt(count)
+                    .childAt(1)
+                    .type(),
+                ).toEqual(Form.Group);
+
+                let subFieldCounter = 0;
+                field.fields.forEach((input) => {
+                  const rowInput = wrapper
+                    .childAt(1)
+                    .childAt(0)
+                    .childAt(count)
+                    .childAt(1)
+                    .childAt(subFieldCounter);
+
+                  switch (input.type) {
+                    case 'input':
+                      expect(rowInput.type()).toEqual(Input);
+                      break;
+                    case 'dropDown':
+                      expect(rowInput.type()).toEqual(Dropdown);
+                      break;
+                    case 'textArea':
+                      expect(rowInput.type()).toEqual(TextArea);
+                      break;
+                    case 'checkbox':
+                      expect(rowInput.type()).toEqual(Checkbox);
+                      break;
+                    case 'radio':
+                      expect(rowInput.type()).toEqual(Checkbox);
+                      break;
+                    default:
+                  }
+                  ifailTest(wrapper, field, count);
+                  subFieldCounter += 1;
+                });
+              }
+
+              if (field.type === 'input') {
+                expect(
+                  wrapper
+                    .childAt(1)
+                    .childAt(0)
+                    .childAt(count)
+                    .childAt(1)
+                    .type(),
+                ).toEqual(Input);
+
+                failTest(wrapper, field, count);
+              }
+              count += 1;
+            });
+          }
+        });
+      });
+
+      describe('Form Button', () => {
+        let formButton;
+
+        beforeEach(() => {
+          formButton = formComponent.childAt(numberOfFields);
+        });
+
+        it('It is a button', () => {
+          console.log(formComponent.debug());
+          console.log(numberOfFields);
+          expect(formButton.type()).toEqual(Form.Button);
+        });
+      });
     });
   });
 });
