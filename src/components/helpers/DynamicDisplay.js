d67a2692f07d6cf382605ea566edca425e582160
Shane Drafahl <shanedrafahl@gmail.com>
2018-07-27 20:25:08 -0500

    Got started on dynamic display

--- src/components/helpers/DynamicDisplay.js
+++ src/components/helpers/DynamicDisplay.js
@@ -1,5 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import {
+  Form,
+  Header,
+  Segment,
+  Checkbox,
+  TextArea,
+  Dropdown,
+  Popup,
+  Icon,
+} from 'semantic-ui-react';
+import { statekeys } from '../../helpers/Common';
+import Styles from '../../styles/DynamicForm';
+import InlineError from './InlineError';
 
-const DynamicDisplay = () => (<p>display</p>);
+class DynamicDisplay extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
 
+  render() {
+    return (
+      <Segment>
+        DynamicDisplay
+      </Segment>
+    );
+  }
+}
 export default DynamicDisplay;
--- test/components/helpers/reviewModal.spec.js
+++ test/components/helpers/reviewModal.spec.js
@@ -1,42 +1,42 @@
-import { shallow } from "enzyme";
-import { Menu, Modal, Grid } from "semantic-ui-react";
-import React from "react";
-import reviewModal from "../src/components/helpers/reviewModal";
+import { shallow } from 'enzyme';
+import { Menu, Modal, Grid } from 'semantic-ui-react';
+import React from 'react';
+import reviewModal from '../../../src/components/helpers/reviewModal';
 
-describe("pipelineSignupModal", () => {
+describe('pipelineSignupModal', () => {
   let wrapper
 
   const renderComponent = (props = {}) =>
     shallow(
-      <reviewModal/>
+      <reviewModal />
     );
 
   beforeEach(() => {
     wrapper = renderComponent();
   });
 
-  it("is wrapped in a modal", () => {
+  it('is wrapped in a modal', () => {
     expect(wrapper.type()).toEqual(Modal);
   });
 
-  describe("modal header", () => {
+  describe('modal header', () => {
     let modalHeader;
 
     beforeEach(() => {
       modalHeader = wrapper.childAt(0);
     });
 
-    it("is a modal header", () => {
+    it('is a modal header', () => {
       expect(modalHeader.type()).toEqual(Modal.Header);
     });
   });
-    describe("Modal content", () => {
+    describe('Modal content', () => {
       let modalContent;
 
       beforeEach(() => {
         modalContent = wrapper.childAt(1);
       });
-      it("is modal content", () => {
+      it('is modal content', () => {
         expect(modalContent.type()).toEqual(Modal.Content);
       });
 });
