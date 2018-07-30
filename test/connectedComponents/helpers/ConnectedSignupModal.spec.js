7b224ff0b588a533a0ae376be7a3e09726344a57
Ben Trettin <batrettin@gmail.com>
2018-07-30 12:45:31 -0500

    refactored tests for navbar and login/signup modals as well as created their connected component test

--- test/connectedComponents/helpers/ConnectedSignupModal.spec.js
+++ test/connectedComponents/helpers/ConnectedSignupModal.spec.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import configureStore from 'redux-mock-store';
-import NavBar from '../../src/connectedComponents/ConnectedNavBar';
+import SignupModal from '../../../src/connectedComponents/helpers/ConnectedSignupModal';
 
 describe('Connected NavBar', () => {
   let store;
@@ -14,9 +14,9 @@ describe('Connected NavBar', () => {
   });
 
   it('has the correct props', () => {
-    const wrapper = shallow(<NavBar store={store} />);
+    const wrapper = shallow(<SignupModal store={store} />);
 
-    const dispatchProps = ['login', 'signup'];
+    const dispatchProps = ['signup'];
 
     dispatchProps.forEach((prop) => {
       expect(wrapper.props()[prop]).toEqual(expect.any(Function));
