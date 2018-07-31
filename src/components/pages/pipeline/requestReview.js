2aac3a6a70fbed6c9f6a3a306b90c9512037602a
Shane Drafahl <shanedrafahl@gmail.com>
2018-07-30 23:08:55 -0500

    Added inline error

--- src/components/pages/pipeline/requestReview.js
+++ src/components/pages/pipeline/requestReview.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Grid, Segment } from 'semantic-ui-react';
 import { Redirect } from 'react-router';
 import PropTypes from 'prop-types';
@@ -6,29 +6,44 @@ import Styles from '../../../styles/pipeline/requestReview';
 import BreadcrumbComponent from '../../helpers/breadcrumb';
 import DynamicDisplayComponent from '../../helpers/DynamicDisplay';
 import SubmitModalComponent from '../../helpers/reviewModal';
+import InlineError from '../../helpers/InlineError';
 
-export const requestReview = (props) => {
-  if (props.pageInProgress < 4) {
-    return <Redirect to="/pipeline/requestPrice" />;
+class requestReview extends Component {
+  constructor() {
+    super();
+    this.state = {
+      dataBaseError: null,
+    };
   }
-  return (
-    <Grid centered container style={Styles.grid}>
-      <Grid.Row style={Styles.breadrow}>
-        <Segment style={Styles.segment}>
-          <BreadcrumbComponent selection={props.pageInProgress} current={4} />
-        </Segment>
-      </Grid.Row>
-      <Grid.Row>
-        <DynamicDisplayComponent requests={props.requests} />
-      </Grid.Row>
-      <Grid.Row style={Styles.submitRow}>
-        <Segment style={Styles.buttonSegment}>
-          <SubmitModalComponent />
-        </Segment>
-      </Grid.Row>
-    </Grid>
-  );
-};
+
+  render() {
+    if (this.props.pageInProgress < 4) {
+      return <Redirect to="/pipeline/requestPrice" />;
+    }
+    return (
+      <Grid centered container style={Styles.grid}>
+        <Grid.Row style={Styles.breadrow}>
+          <Segment style={Styles.segment}>
+            <BreadcrumbComponent selection={this.props.pageInProgress} current={4} />
+          </Segment>
+        </Grid.Row>
+        {this.state.dataBaseError &&
+             (<Grid.Row>
+               <InlineError text={this.state.dataBaseError} pointing />
+             </Grid.Row>)}
+        <Grid.Row>
+          <DynamicDisplayComponent requests={this.props.requests} />
+        </Grid.Row>
+        <Grid.Row style={Styles.submitRow}>
+          <Segment style={Styles.buttonSegment}>
+            <SubmitModalComponent />
+          </Segment>
+        </Grid.Row>
+      </Grid>
+    );
+  }
+}
+
 requestReview.propTypes = {
   pageInProgress: PropTypes.number,
   requests: PropTypes.obj,
