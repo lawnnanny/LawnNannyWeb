2773e8a60092708caf249764c34619822805254e
Ben Trettin <batrettin@gmail.com>
2018-08-04 15:15:34 -0500

    mapping an object to create the cards

--- src/components/pages/Requests.js
+++ src/components/pages/Requests.js
@@ -4,22 +4,61 @@ import Styles from '../../styles/Requests';
 import RequestCardComponent from '../helpers/requestCard';
 import lawn from '../../images/homepage/lawn.jpg';
 
-const Requests = () => (
-  <Grid stackable centered style={Styles.grid}>
-    <Grid.Row verticalAlign="middle">
-      <Search placeholder="Type a location" />
-    </Grid.Row>
-    <Grid.Row centered>
-      <RequestCardComponent
-        image={lawn}
-        header={'Lawn in Newton'}
-        distance={'10 miles away'}
-        description={'Full yard with a few trees scattered'}
-        price={'25'}
-        reviewCount={'86'}
-        rating={4}
-      />
-    </Grid.Row>
-  </Grid>
-);
+const jsonForm = {
+  fields: [
+    {
+      image: lawn,
+      header: 'Lawn in Newton',
+      distance: '10 miles away',
+      description: 'Full yard with a few trees scattered',
+      price: '25',
+      reviewCount: '86',
+      rating: 4,
+    },
+    {
+      image: lawn,
+      header: 'Lawn in Newton',
+      distance: '10 miles away',
+      description: 'Full yard with a few trees scattered',
+      price: '25',
+      reviewCount: '86',
+      rating: 2,
+    },
+    {
+      image: lawn,
+      header: 'Lawn in Newton',
+      distance: '10 miles away',
+      description: 'Full yard with a few trees scattered',
+      price: '25',
+      reviewCount: '86',
+      rating: 3,
+    },
+  ],
+};
+
+const Requests = () => {
+  const renderCard = field => (
+    <RequestCardComponent
+      image={field.image}
+      header={field.header}
+      distance={field.distance}
+      description={field.description}
+      price={field.price}
+      reviewCount={field.reviewCount}
+      rating={field.rating}
+    />
+  );
+  const renderCards = (form) => {
+    const formUI = form.fields.map(field => renderCard(field));
+    return formUI;
+  };
+  return (
+    <Grid stackable centered style={Styles.grid}>
+      <Grid.Row verticalAlign="middle">
+        <Search placeholder="Type a location" />
+      </Grid.Row>
+      <Grid.Row centered>{renderCards(jsonForm)}</Grid.Row>
+    </Grid>
+  );
+};
 export default Requests;
