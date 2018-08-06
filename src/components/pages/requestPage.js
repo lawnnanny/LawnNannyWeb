627df4377f99d234ddc0104f8fb0b509c5ae5a77
Ben Trettin <batrettin@gmail.com>
2018-08-06 12:54:39 -0500

    added button to bottom of request page

--- src/components/pages/requestPage.js
+++ src/components/pages/requestPage.js
@@ -1,11 +1,40 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Image, Menu, Header, Button, Icon } from 'semantic-ui-react';
 import Styles from '../../styles/requestPage';
 import home from '../../images/nicehome.jpg';
 
 const requestReview = () => (
   <Grid centered container style={Styles.grid}>
+    <Menu fixed="bottom" style={Styles.menu}>
+      <Menu.Menu position="right">
+        <Button style={Styles.requestButton}>Choose Request </Button>
+      </Menu.Menu>
+    </Menu>
     <Grid.Row style={Styles.breadrow}>
+      <Image src={home} />
+    </Grid.Row>
+    <Grid.Row>
+      <Image src={home} />
+    </Grid.Row>
+    <Grid.Row>
+      <Image src={home} />
+    </Grid.Row>
+    <Grid.Row>
+      <Image src={home} />
+    </Grid.Row>
+    <Grid.Row>
+      <Image src={home} />
+    </Grid.Row>
+    <Grid.Row>
+      <Image src={home} />
+    </Grid.Row>
+    <Grid.Row>
+      <Image src={home} />
+    </Grid.Row>
+    <Grid.Row>
+      <Image src={home} />
+    </Grid.Row>
+    <Grid.Row>
       <Image src={home} />
     </Grid.Row>
   </Grid>
---
+++ src/styles/requestPage.js
@@ -0,0 +1,24 @@
+const white = '#FFFFFF';
+const green = '#20ad1d';
+const blue = '#1205ff';
+export default {
+  menu: {
+    height: '8em',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  toTopButton: {
+    height: '2.5em',
+    padding: '.5em',
+    fontSize: '1.5em',
+    backgroundColor: blue,
+    color: white,
+  },
+  requestButton: {
+    height: '2.5em',
+    padding: '.5em',
+    fontSize: '1.5em',
+    backgroundColor: green,
+    color: white,
+  },
+};
