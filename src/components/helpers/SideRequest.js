99006197bf86c78493acf8cf817d8b174ae0f955
Benjamin Trettin <batrettin@gmail.com>
2018-08-07 17:51:18 -0500

    formatted request card

--- src/components/helpers/SideRequest.js
+++ src/components/helpers/SideRequest.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Grid, Image, Header,Rating, Icon , Label} from 'semantic-ui-react';
+import styled from 'styled-components';
 import Styles from '../../styles/helpers/SideRequest';
-import lawn from '../../images/homepage/lawn.jpg';
 
+const HeaderColumn = styled(Grid.Column)`
+  & {
+    margin-top:0 !important;
+    }
+  }
+`;
 const SideRequest = props => (
   <Grid columns='equal'>
   <Grid.Row style={Styles.row}>
@@ -10,11 +16,9 @@ const SideRequest = props => (
   <Icon name='dollar' style={Styles.priceIcon}/>
   {props.price}
   </Label>
-  <Grid.Column style={Styles.column}>
-  <Grid.Row>
+  <HeaderColumn style={Styles.column}>
     <Header style={Styles.header}> {props.header}</Header>
-    </Grid.Row>
-    </Grid.Column>
+    </HeaderColumn>
     <Grid.Column style={Styles.column}>
     <Grid.Row>
     <Rating disabled icon='star' defaultRating={props.rating} maxRating={5} />
