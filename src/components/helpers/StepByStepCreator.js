aaddd1c79fe4f298bb2c986dd883a56d1a321558
Shane Drafahl <shanedrafahl@gmail.com>
2018-08-25 13:17:52 -0500

    Did some work on the transitional Portal

--- src/components/helpers/StepByStepCreator.js
+++ src/components/helpers/StepByStepCreator.js
@@ -1,16 +1,55 @@
 import React, { Component } from 'react';
-import { TransitionablePortal } from 'semantic-ui-react';
+import { TransitionablePortal, Segment, Button } from 'semantic-ui-react';
 
 export class StepBystepCreator extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      open: false,
+      steps: [],
+    };
+  }
+
+  toggleOpen = () => {
+    const state = this.state;
+    state.open = true;
+    this.setState(state);
+  }
+
+  toggleClose = () => {
+    const state = this.state;
+    state.open = false;
+    this.setState(state);
+  }
+
+  renderSteps = () => {
+    this.state.steps.map(listItem => (
+      (
+        <Segment>
+            stuff
+        </Segment>
+      )
+    ));
   }
 
   render() {
     return (
-      <TransitionablePortal>
-        stuff
+      <TransitionablePortal
+        closeOnTriggerClick
+        openOnTriggerClick
+        onOpen={this.toggleOpen}
+        onClose={this.toggleClose}
+        trigger={
+          <Button
+            content={open ? 'Close Portal' : 'Open Portal'}
+            negative={open}
+            positive={!open}
+          />
+        }
+      >
+        <Segment style={{ left: '40%', position: 'fixed', top: '50%', zIndex: 1000 }}>
+          stuff
+        </Segment>
       </TransitionablePortal>
     );
   }
