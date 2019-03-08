3beef442c36166ac440d6df1a5ba6c00e13e7dcf
shane <sdrafahl@iastate.edu>
2019-03-07 19:41:21 -0600

    Got tests to pass

---
+++ src/__tests__/components/helpers/loginModal.spec.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import { Menu, Modal, Button } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import { shallow } from 'enzyme';
+import LoginModal from '../../../../src/components/helpers/loginModal';
+import DynamicComponent from '../../../../src/components/helpers/DynamicForm';
+
+describe('loginModal', () => {
+  let wrapper;
+
+  const renderComponent = () => shallow(<LoginModal />);
+
+  beforeEach(() => {
+    wrapper = renderComponent();
+  });
+
+  it('is a modal', () => {
+    expect(wrapper.type()).toEqual(Modal);
+  });
+
+  it('is a mini modal', () => {
+    expect(wrapper.props().size).toEqual('mini');
+  });
+
+  it('displays a close modal button', () => {
+    expect(wrapper.props().closeIcon).toBeTruthy();
+  });
+
+  describe('Login Modal trigger', () => {
+    let loginModalTrigger;
+
+    beforeEach(() => {
+      loginModalTrigger = shallow(wrapper.props().trigger);
+    });
+
+    it('is a menu item', () => {
+      const expectedType = shallow(<Menu.Item />).type();
+
+      expect(loginModalTrigger.type()).toEqual(expectedType);
+    });
+    describe('Login Modal Button', () => {
+      let loginModalButton;
+      beforeEach(() => {
+        loginModalButton = loginModalTrigger.childAt(0);
+      });
+
+      it('is a modal button', () => {
+        expect(loginModalButton.type()).toEqual(Button);
+      });
+      it('it displays the correct text', () => {
+        const buttonText = loginModalButton.childAt(0);
+        expect(buttonText.text()).toEqual('Login');
+      });
+    });
+  });
+
+  describe('Login Modal Header', () => {
+    let loginModalHeader;
+
+    beforeEach(() => {
+      loginModalHeader = wrapper.childAt(0);
+    });
+
+    it('is a modal header', () => {
+      expect(loginModalHeader.type()).toEqual(Modal.Header);
+    });
+
+    it('displays the correct text', () => {
+      const loginModalHeaderText = loginModalHeader.childAt(0);
+
+      expect(loginModalHeaderText.text()).toEqual('Welcome Back!');
+    });
+  });
+
+  describe('Login Modal Description', () => {
+    let loginModalDescription;
+
+    beforeEach(() => {
+      loginModalDescription = wrapper.childAt(1);
+    });
+
+    it('is a modal description', () => {
+      expect(loginModalDescription.type()).toEqual(Modal.Description);
+    });
+
+    describe('Dynamic Form Description', () => {
+      let loginDescriptionForm;
+
+      beforeEach(() => {
+        loginDescriptionForm = loginModalDescription.childAt(1);
+      });
+
+      it('is a dynamic component', () => {
+        expect(loginDescriptionForm.type()).toEqual(DynamicComponent);
+      });
+    });
+  });
+});
---
+++ src/__tests__/connectedComponents/ConnectedNavBar.spec.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import configureStore from 'redux-mock-store';
+import NavBar from '../../../src/connectedComponents/ConnectedNavBar';
+
+describe('Connected RequestInformation Page', () => {
+  let store;
+
+  beforeEach(() => {
+    const state = {
+      login: {
+        isLoggedIn: false,
+      },
+    };
+
+    const mockStore = configureStore();
+    store = mockStore(state);
+  });
+
+  it('has the correct props', () => {
+    const wrapper = shallow(<NavBar store={store} />);
+
+    const dispatchProps = ['setLoggedIn'];
+
+    dispatchProps.forEach((prop) => {
+      expect(wrapper.props()[prop]).toEqual(expect.any(Function));
+    });
+  });
+});
