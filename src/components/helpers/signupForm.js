cd90e919f966033f998a4a9437b68ec5e5b2edfd
Ben Trettin <batrettin@gmail.com>
2019-05-29 14:09:13 -0500

    added error displays

--- src/components/helpers/signupForm.js
+++ src/components/helpers/signupForm.js
@@ -7,30 +7,24 @@ import InputLabel from '@material-ui/core/InputLabel';
 import styles from './signupForm.module.css';
 
 const initialValues = {
-  Login: [
-    {
-      firstName: '',
-      lastName: '',
-      email: '',
-      password: '',
-      repeatPassword: '',
-    },
-  ],
+  firstName: '',
+  lastName: '',
+  email: '',
+  password: '',
+  repeatPassword: '',
 };
 
 const SignupFormComponent = props => (
   <div className={styles.formContainer}>
     <Formik
       validationSchema={Yup.object({
-        Login: Yup.array().of(
-          Yup.object({
-            firstName: Yup.string().required('Required*'),
-            lastName: Yup.string().required('Required*'),
-            email: Yup.string().required('Required*'),
-            password: Yup.string().required('Required*'),
-            repeatPassword: Yup.string().required('Required*'),
-          }),
-        ),
+        firstName: Yup.string().required('Required*'),
+        lastName: Yup.string().required('Required*'),
+        email: Yup.string()
+          .email('Not a valid email address')
+          .required('Required*'),
+        password: Yup.string().required('Required*'),
+        repeatPassword: Yup.string().required('Required*'),
       })}
       initialValues={initialValues}
       onSubmit={(values, { setSubmitting }) => {
@@ -41,87 +35,138 @@ const SignupFormComponent = props => (
       }}
     >
       {({ values, error, touched, isSubmitting }) => (
-        <Form>
-          <FieldArray name="Login">
-            {({ push, remove }) => (
-              <React.Fragment>
-                {values.Login &&
-                  values.Login.length > 0 &&
-                  values.Login.map((friend, index) => (
-                    <React.Fragment>
-                      <div>
-                        <Field name={`Login[${index}].firstName`} type="text">
-                          {({ field, form }) => (
-                            <FormControl className={styles.addressContainer}>
-                              <InputLabel className={styles.fieldLabel}>First Name </InputLabel>
-                              <Input className={styles.input} {...field} />
-                            </FormControl>
-                          )}
-                        </Field>
-                        <ErrorMessage name={`Login[${index}].firstName`}>
-                          {msg => <div className={styles.inputError}>{msg}</div>}
-                        </ErrorMessage>
-                      </div>
-                      <div>
-                        <Field name={`Login[${index}].lastName`} type="text">
-                          {({ field, form }) => (
-                            <FormControl className={styles.addressContainer}>
-                              <InputLabel className={styles.fieldLabel}>Last Name </InputLabel>
-                              <Input className={styles.input} {...field} />
-                            </FormControl>
-                          )}
-                        </Field>
-                        <ErrorMessage name={`Login[${index}].lastName`}>
-                          {msg => <div className={styles.inputError}>{msg}</div>}
-                        </ErrorMessage>
-                      </div>
-                      <div>
-                        <Field name={`Login[${index}].email`} type="text">
-                          {({ field, form }) => (
-                            <FormControl className={styles.addressContainer}>
-                              <InputLabel className={styles.fieldLabel}>Email </InputLabel>
-                              <Input className={styles.input} {...field} />
-                            </FormControl>
-                          )}
-                        </Field>
-                        <ErrorMessage name={`Login[${index}].email`}>
-                          {msg => <div className={styles.inputError}>{msg}</div>}
-                        </ErrorMessage>
-                      </div>
-                      <div>
-                        <Field name={`Login[${index}].password`} type="text">
-                          {({ field, form }) => (
-                            <FormControl className={styles.addressContainer}>
-                              <InputLabel className={styles.fieldLabel}>Password</InputLabel>
-                              <Input type="password" className={styles.input} {...field} />
-                            </FormControl>
-                          )}
-                        </Field>
-                        <ErrorMessage name={`Login[${index}].password`}>
-                          {msg => <div className={styles.inputError}>{msg}</div>}
-                        </ErrorMessage>
-                      </div>
-                      <div>
-                        <Field name={`Login[${index}].repeatPassword`} type="text">
-                          {({ field, form }) => (
-                            <FormControl className={styles.addressContainer}>
-                              <InputLabel className={styles.fieldLabel}>Repeat Password</InputLabel>
-                              <Input type="password" className={styles.input} {...field} />
-                            </FormControl>
-                          )}
-                        </Field>
-                        <ErrorMessage name={`Login[${index}].repeatPassword`}>
-                          {msg => <div className={styles.inputError}>{msg}</div>}
-                        </ErrorMessage>
-                      </div>
-                    </React.Fragment>
-                  ))}
-                <button className={styles.submitButton} type="submit">
-                  Sign Up
-                </button>
-              </React.Fragment>
-            )}
-          </FieldArray>
+        <Form className={styles.form}>
+          <div>
+            <Field name="firstName" type="text">
+              {({ field, form }) => (
+                <FormControl className={styles.addressContainer}>
+                  <InputLabel
+                    className={
+                      form.touched[field.name] && form.errors[field.name]
+                        ? styles.fieldLabelError
+                        : styles.fieldLabel
+                    }
+                  >
+                    First Name{' '}
+                  </InputLabel>
+                  <Input
+                    error={form.touched[field.name] && form.errors[field.name]}
+                    className={styles.input}
+                    {...field}
+                  />
+                </FormControl>
+              )}
+            </Field>
+            <ErrorMessage name="firstName">
+              {msg => <div className={styles.inputError}>{msg}</div>}
+            </ErrorMessage>
+          </div>
+          <div>
+            <Field name="lastName" type="text">
+              {({ field, form }) => (
+                <FormControl className={styles.addressContainer}>
+                  <InputLabel
+                    className={
+                      form.touched[field.name] && form.errors[field.name]
+                        ? styles.fieldLabelError
+                        : styles.fieldLabel
+                    }
+                  >
+                    Last Name{' '}
+                  </InputLabel>
+                  <Input
+                    error={form.touched[field.name] && form.errors[field.name]}
+                    className={styles.input}
+                    {...field}
+                  />
+                </FormControl>
+              )}
+            </Field>
+            <ErrorMessage name="lastName">
+              {msg => <div className={styles.inputError}>{msg}</div>}
+            </ErrorMessage>
+          </div>
+          <div>
+            <Field name="email" type="text">
+              {({ field, form }) => (
+                <FormControl className={styles.addressContainer}>
+                  <InputLabel
+                    className={
+                      form.touched[field.name] && form.errors[field.name]
+                        ? styles.fieldLabelError
+                        : styles.fieldLabel
+                    }
+                  >
+                    Email{' '}
+                  </InputLabel>
+                  <Input
+                    error={form.touched[field.name] && form.errors[field.name]}
+                    className={styles.input}
+                    {...field}
+                  />
+                </FormControl>
+              )}
+            </Field>
+            <ErrorMessage name="email">
+              {msg => <div className={styles.inputError}>{msg}</div>}
+            </ErrorMessage>
+          </div>
+          <div>
+            <Field name="password" type="text">
+              {({ field, form }) => (
+                <FormControl className={styles.addressContainer}>
+                  <InputLabel
+                    className={
+                      form.touched[field.name] && form.errors[field.name]
+                        ? styles.fieldLabelError
+                        : styles.fieldLabel
+                    }
+                  >
+                    Password
+                  </InputLabel>
+                  <Input
+                    error={form.touched[field.name] && form.errors[field.name]}
+                    type="password"
+                    className={styles.input}
+                    {...field}
+                  />
+                </FormControl>
+              )}
+            </Field>
+            <ErrorMessage name="password">
+              {msg => <div className={styles.inputError}>{msg}</div>}
+            </ErrorMessage>
+          </div>
+          <div>
+            <Field name="repeatPassword" type="text">
+              {({ field, form }) => (
+                <FormControl className={styles.addressContainer}>
+                  <InputLabel
+                    className={
+                      form.touched[field.name] && form.errors[field.name]
+                        ? styles.fieldLabelError
+                        : styles.fieldLabel
+                    }
+                  >
+                    Repeat Password
+                  </InputLabel>
+                  <Input
+                    error={form.touched[field.name] && form.errors[field.name]}
+                    type="password"
+                    className={styles.input}
+                    {...field}
+                  />
+                </FormControl>
+              )}
+            </Field>
+            <ErrorMessage name="repeatPassword">
+              {msg => <div className={styles.inputError}>{msg}</div>}
+            </ErrorMessage>
+          </div>
+
+          <button className={styles.submitButton} type="submit">
+            Sign Up
+          </button>
         </Form>
       )}
     </Formik>
